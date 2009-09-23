1263000ecdea009f3ab1a9f831a23f3770540324
Jonas Abreu <jonas@vidageek.net>
2009-09-23 19:28:08 -0300

    moved element creation logic to TagType

--- src/main/java/br/com/adaptworks/scraper/element/ElementParser.java
+++ src/main/java/br/com/adaptworks/scraper/element/ElementParser.java
@@ -5,10 +5,8 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import br.com.adaptworks.scraper.tag.BangTagElement;
 import br.com.adaptworks.scraper.tag.Tag;
 import br.com.adaptworks.scraper.tag.TagReader;
-import br.com.adaptworks.scraper.tag.TagType;
 
 /**
  * @author jonasabreu
@@ -16,31 +14,28 @@ import br.com.adaptworks.scraper.tag.TagType;
  */
 final public class ElementParser {
 
-	private final Pattern pattern;
+    private final Pattern pattern;
 
-	public ElementParser() {
-		this.pattern = Pattern.compile("(?s)<([^<>]*?)>([^<>]*)");
-	}
+    public ElementParser() {
+        pattern = Pattern.compile("(?s)<([^<>]*?)>([^<>]*)");
+    }
 
-	public List<Element> parse(final String template) {
-		ArrayList<Element> elements = new ArrayList<Element>();
-		Matcher matcher = this.pattern.matcher(template);
-		while (matcher.find()) {
-			Tag tag = new TagReader().readTag(matcher.group(1));
-			String elementContent = null;
-			if (matcher.group(2).length() != 0) {
-				elementContent = matcher.group(2);
-			}
-			if (tag.type().equals(TagType.OPEN)) {
-				elements.add(new OpenTagElement(tag, elementContent));
-			} else if (tag.type().equals(TagType.CLOSE)) {
-				elements.add(new CloseTagElement(tag, elementContent));
-			} else {
-				elements.add(new BangTagElement());
-			}
-		}
+    public List<Element> parse(final String template) {
+        ArrayList<Element> elements = new ArrayList<Element>();
+        Matcher matcher = pattern.matcher(template);
+        while (matcher.find()) {
+            Tag tag = new TagReader().readTag(matcher.group(1));
+            String elementContent = null;
 
-		return elements;
-	}
+            if (matcher.group(2).length() != 0) {
+                elementContent = matcher.group(2);
+            }
+
+            elements.add(tag.type().createElement(tag, elementContent));
+
+        }
+
+        return elements;
+    }
 
 }
--- src/main/java/br/com/adaptworks/scraper/tag/TagType.java
+++ src/main/java/br/com/adaptworks/scraper/tag/TagType.java
@@ -1,26 +1,52 @@
 package br.com.adaptworks.scraper.tag;
 
+import br.com.adaptworks.scraper.element.CloseTagElement;
+import br.com.adaptworks.scraper.element.Element;
+import br.com.adaptworks.scraper.element.OpenTagElement;
+
 public enum TagType {
-	OPEN(' '), CLOSE('/'), BANG('!');
 
-	private final char character;
+    OPEN(' ') {
+        @Override
+        public Element createElement(final Tag tag, final String elementContent) {
+            return new OpenTagElement(tag, elementContent);
+        }
+    },
 
-	private TagType(final char character) {
-		this.character = character;
+    CLOSE('/') {
+        @Override
+        public Element createElement(final Tag tag, final String elementContent) {
+            return new CloseTagElement(tag, elementContent);
+        }
+    },
 
-	}
+    BANG('!') {
+        @Override
+        public Element createElement(final Tag tag, final String elementContent) {
+            return new BangTagElement();
+        }
+    };
 
-	public static TagType fromValue(final char character) {
-		for (TagType type : values()) {
-			if (type.character == character) {
-				return type;
-			}
-		}
-		return TagType.OPEN;
-	}
+    private final char character;
 
-	@Override
-	public String toString() {
-		return "" + this.character;
-	}
+    private TagType(final char character) {
+        this.character = character;
+
+    }
+
+    public static TagType fromValue(final char character) {
+        for (TagType type : values()) {
+            if (type.character == character) {
+                return type;
+            }
+        }
+        return TagType.OPEN;
+    }
+
+    public abstract Element createElement(Tag tag, String elementContent);
+
+    @Override
+    public String toString() {
+        return "" + character;
+    }
 }
