535d519c35c1b8800778ae871c61f5cd30ead9a4
Jonas Abreu <jonas@vidageek.net>
2009-09-23 19:33:18 -0300

    changing variable name

--- src/main/java/br/com/adaptworks/scraper/tag/TagReader.java
+++ src/main/java/br/com/adaptworks/scraper/tag/TagReader.java
@@ -9,53 +9,53 @@ import java.util.Map;
  */
 final public class TagReader {
 
-	public Tag readTag(final String declaration) {
-		String tag = new TagSanitizer().sanitize(declaration);
-		Map<String, String> attributes = this.recoverAttributes(tag);
-		String tagName = this.recoverName(tag);
-		TagType tagType = this.recoverType(tag);
+    public Tag readTag(final String declaration) {
+        String tag = new TagSanitizer().sanitize(declaration);
+        Map<String, String> attributes = recoverAttributes(tag);
+        String tagName = recoverName(tag);
+        TagType tagType = recoverType(tag);
 
-		return new Tag(tagName, tagType, attributes);
-	}
+        return new Tag(tagName, tagType, attributes);
+    }
 
-	private TagType recoverType(final String tag) {
-		return TagType.fromValue(tag.charAt(0));
-	}
+    private TagType recoverType(final String tag) {
+        return TagType.fromValue(tag.charAt(0));
+    }
 
-	private String recoverName(final String tag) {
-		String tagName = tag.substring(0, tag.indexOf(" ") == -1 ? tag.length() : tag.indexOf(" "));
+    private String recoverName(final String tag) {
+        String tagName = tag.substring(0, tag.indexOf(" ") == -1 ? tag.length() : tag.indexOf(" "));
 
-		return tagName.startsWith("/") ? tagName.substring(1) : tagName;
-	}
+        return tagName.startsWith("/") ? tagName.substring(1) : tagName;
+    }
 
-	private Map<String, String> recoverAttributes(final String declaration) {
-		String[] tokens = this.tokenize(declaration);
-		Map<String, String> map = new HashMap<String, String>();
+    private Map<String, String> recoverAttributes(final String declaration) {
+        String[] tokens = tokenize(declaration);
+        Map<String, String> map = new HashMap<String, String>();
 
-		for (int i = 1; i < tokens.length; i++) {
-			String[] attribute = tokens[i].split("=\"|\"");
-			if (attribute.length == 2) {
-				map.put(attribute[0], attribute[1]);
-			}
-		}
-		return map;
-	}
+        for (int i = 1; i < tokens.length; i++) {
+            String[] attribute = tokens[i].split("=\"|\"");
+            if (attribute.length == 2) {
+                map.put(attribute[0], attribute[1]);
+            }
+        }
+        return map;
+    }
 
-	private String[] tokenize(final String declaration) {
-		boolean dentroDeAspas = false;
-		String temp = "";
+    private String[] tokenize(final String declaration) {
+        boolean insideAttributeValue = false;
+        String safeDeclaration = "";
 
-		for (int i = 0; i < declaration.length(); i++) {
-			if (declaration.charAt(i) == '"') {
-				dentroDeAspas = !dentroDeAspas;
-			}
-			if ((declaration.charAt(i) == ' ') && !dentroDeAspas) {
-				temp += '#';
-			} else {
-				temp += declaration.charAt(i);
-			}
-		}
-		return temp.split("#");
-	}
+        for (int i = 0; i < declaration.length(); i++) {
+            if (declaration.charAt(i) == '"') {
+                insideAttributeValue = !insideAttributeValue;
+            }
+            if ((declaration.charAt(i) == ' ') && !insideAttributeValue) {
+                safeDeclaration += '#';
+            } else {
+                safeDeclaration += declaration.charAt(i);
+            }
+        }
+        return safeDeclaration.split("#");
+    }
 
 }
