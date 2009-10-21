b887c3ba16d5ec0072aab8c52ffd4c295b2cc051
Jonas Abreu <jonas@vidageek.net>
2009-10-21 17:14:20 -0200

    fixed tag cleaner

--- src/main/java/br/com/adaptworks/scraper/cleaner/IrrelevantTagCleaner.java
+++ src/main/java/br/com/adaptworks/scraper/cleaner/IrrelevantTagCleaner.java
@@ -2,6 +2,7 @@ package br.com.adaptworks.scraper.cleaner;
 
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.regex.Pattern;
 
 import br.com.adaptworks.scraper.tag.Tag;
 
@@ -11,36 +12,38 @@ import br.com.adaptworks.scraper.tag.Tag;
  */
 final public class IrrelevantTagCleaner implements TagCleaner {
 
-	private final List<Tag> relevantTags;
+    private final List<Tag> relevantTags;
 
-	public IrrelevantTagCleaner(final List<Tag> relevantTags) {
-		this.relevantTags = relevantTags;
-	}
+    public IrrelevantTagCleaner(final List<Tag> relevantTags) {
+        this.relevantTags = relevantTags;
+    }
 
-	public boolean shouldClean(final Tag element) {
-		for (Tag tag : relevantTags) {
-			if (tag.name().equals(element.name()) && tag.type().equals(element.type())
-					&& attributesMatches(tag, element) && contentMatches(tag, element)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public boolean shouldClean(final Tag element) {
+        for (Tag tag : relevantTags) {
+            if (tag.name().equals(element.name()) && tag.type().equals(element.type())
+                    && attributesMatches(tag, element) && contentMatches(tag, element)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	private boolean attributesMatches(final Tag tag, final Tag element) {
-		for (Entry<String, String> entry : tag.attributes().entrySet()) {
-			if (!element.attributes().containsKey(entry.getKey())
-					|| !element.attributes().get(entry.getKey()).contains(entry.getValue())) {
-				return false;
-			}
-		}
-		return true;
-	}
+    private boolean attributesMatches(final Tag tag, final Tag element) {
+        for (Entry<String, String> entry : tag.attributes().entrySet()) {
+            if (!element.attributes().containsKey(entry.getKey())
+                    || !element.attributes().get(entry.getKey()).contains(entry.getValue())) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	private boolean contentMatches(final Tag tag, final Tag element) {
-		if ("".equals(tag.content().trim())) {
-			return true;
-		}
-		return "".equals(tag.content().trim()) ? true : tag.content().equals(element.content());
-	}
+    private boolean contentMatches(final Tag tag, final Tag element) {
+        if ("".equals(tag.content().trim())) {
+            return true;
+        }
+        String regex = "(?s)(?i)\\Q" + tag.content().replaceAll("(\\$\\{.*?\\})", "\\\\E.*?\\\\Q") + "\\E";
+        System.out.println(regex);
+        return Pattern.compile(regex).matcher(element.content()).matches();
+    }
 }
